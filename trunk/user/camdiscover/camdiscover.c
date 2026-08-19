0be2b46646053b33888e82c70c3570b7dc659fb7
671792320 <671792320@qq.com>
2026-08-19 11:31:54 +0800

    fix: bind ONVIF discovery socket and select multicast interface

--- trunk/user/camdiscover/camdiscover.c
+++ trunk/user/camdiscover/camdiscover.c
@@ -3,7 +3,7 @@
  *
  * First stage implements ONVIF WS-Discovery without external libraries.
  * The program deliberately only reports discoveries; network configuration
- * belongs to the future netmgr component.
+ * belongs to the future q7netmgr component.
  */
 #include <arpa/inet.h>
 #include <errno.h>
@@ -16,6 +16,7 @@
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/time.h>
+#include <time.h>
 #include <unistd.h>
 
 #define ONVIF_ADDR "239.255.255.250"
@@ -56,21 +57,28 @@ static void extract_xml_value(const char *xml, const char *tag, char *out, size_
 
 static int send_probe(int fd, const struct sockaddr_in *dst)
 {
-    static const char probe[] =
+    char probe[2048];
+    unsigned long now = (unsigned long)time(NULL);
+    int n;
+
+    n = snprintf(probe, sizeof(probe),
         "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
         "<e:Envelope xmlns:e=\"http://www.w3.org/2003/05/soap-envelope\""
         " xmlns:w=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\""
         " xmlns:d=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\""
         " xmlns:dn=\"http://www.onvif.org/ver10/network/wsdl\">"
         "<e:Header>"
-        "<w:MessageID>uuid:camdiscover-q7</w:MessageID>"
+        "<w:MessageID>uuid:camdiscover-%lu</w:MessageID>"
         "<w:To>urn:schemas-xmlsoap-org:ws:2005:04:discovery</w:To>"
         "<w:Action>http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</w:Action>"
         "</e:Header>"
         "<e:Body><d:Probe><d:Types>dn:NetworkVideoTransmitter</d:Types>"
-        "<d:Scopes/></d:Probe></e:Body></e:Envelope>";
+        "<d:Scopes/></d:Probe></e:Body></e:Envelope>", now);
 
-    return (int)sendto(fd, probe, sizeof(probe) - 1, 0,
+    if (n < 0 || n >= (int)sizeof(probe))
+        return -1;
+
+    return (int)sendto(fd, probe, (size_t)n, 0,
                        (const struct sockaddr *)dst, sizeof(*dst));
 }
 
@@ -124,10 +132,11 @@ int main(int argc, char **argv)
 {
     int fd, timeout = DEFAULT_TIMEOUT, opt;
     const char *ifname = NULL;
-    struct sockaddr_in dst;
+    struct sockaddr_in bind_addr, dst;
     struct ip_mreqn mreq;
     unsigned int ifindex = 0;
     int ttl = 1;
+    int reuse = 1;
 
     while ((opt = getopt(argc, argv, "i:t:h")) != -1) {
         switch (opt) {
@@ -152,6 +161,19 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+        perror("SO_REUSEADDR");
+
+    memset(&bind_addr, 0, sizeof(bind_addr));
+    bind_addr.sin_family = AF_INET;
+    bind_addr.sin_port = htons(ONVIF_PORT);
+    bind_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(fd, (struct sockaddr *)&bind_addr, sizeof(bind_addr)) < 0) {
+        perror("bind");
+        close(fd);
+        return 1;
+    }
+
     if (ifname) {
         ifindex = if_nametoindex(ifname);
         if (!ifindex) {
@@ -173,12 +195,6 @@ int main(int argc, char **argv)
 
     if (setsockopt(fd, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl)) < 0)
         perror("IP_MULTICAST_TTL");
-    if (ifname && setsockopt(fd, IPPROTO_IP, IP_MULTICAST_IF,
-                             &(struct in_addr){mreq.imr_address},
-                             sizeof(struct in_addr)) < 0) {
-        /* Some old kernels require an address rather than ifindex here.
-         * Membership above is still sufficient for receiving replies. */
-    }
 
     memset(&dst, 0, sizeof(dst));
     dst.sin_family = AF_INET;
