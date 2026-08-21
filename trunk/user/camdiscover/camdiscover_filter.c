adc883cba2e82797cd6848c65ff57dc408adc8f0
671792320 <671792320@qq.com>
2026-08-21 23:43:48 +0800

    q7: use kernel if header in discovery filter

--- trunk/user/camdiscover/camdiscover_filter.c
+++ trunk/user/camdiscover/camdiscover_filter.c
@@ -1,6 +1,6 @@
 /* Wrapper for camdiscover: hide the router's own IP/MAC from discovery output. */
 #include <arpa/inet.h>
-#include <net/if.h>
+#include <linux/if.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,90 +12,57 @@
 #define MAX_LOCAL 32
 #define LINE_SIZE 2048
 
-struct local_addr {
-    char ip[INET_ADDRSTRLEN];
-    char mac[32];
-};
-
-static struct local_addr local_addr[MAX_LOCAL];
-static int local_count;
+static char local_ips[MAX_LOCAL][INET_ADDRSTRLEN];
+static int local_ip_count;
+static char local_macs[MAX_LOCAL][32];
+static int local_mac_count;
 
 static void add_local_ip(const char *ip)
 {
     int i;
     if (!ip || !*ip || !strcmp(ip, "0.0.0.0")) return;
-    for (i = 0; i < local_count; i++)
-        if (!strcmp(local_addr[i].ip, ip)) return;
-    if (local_count < MAX_LOCAL) {
-        strncpy(local_addr[local_count].ip, ip, sizeof(local_addr[local_count].ip) - 1);
-        local_addr[local_count].ip[sizeof(local_addr[local_count].ip) - 1] = 0;
-        local_count++;
+    for (i = 0; i < local_ip_count; i++)
+        if (!strcmp(local_ips[i], ip)) return;
+    if (local_ip_count < MAX_LOCAL) {
+        strncpy(local_ips[local_ip_count], ip, sizeof(local_ips[0]) - 1);
+        local_ips[local_ip_count][sizeof(local_ips[0]) - 1] = 0;
+        local_ip_count++;
     }
 }
 
-static void add_local_if(const char *ifname)
+static void collect_if(const char *ifname)
 {
-    int fd;
+    int fd, i;
     struct ifreq ifr;
     char ip[INET_ADDRSTRLEN];
+    unsigned char *m;
     char mac[32];
 
     if (!ifname || !*ifname) return;
     fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (fd < 0) return;
 
-    memset(&ifr, 0, sizeof(ifr));
-    strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
-    if (ioctl(fd, SIOCGIFADDR, &ifr) == 0) {
-        struct sockaddr_in *sin = (struct sockaddr_in *)&ifr.ifr_addr;
-        if (inet_ntop(AF_INET, &sin->sin_addr, ip, sizeof(ip)))
-            add_local_ip(ip);
-    }
-
-    memset(&ifr, 0, sizeof(ifr));
-    strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
-    if (ioctl(fd, SIOCGIFHWADDR, &ifr) == 0) {
-        unsigned char *m = (unsigned char *)ifr.ifr_hwaddr.sa_data;
-        snprintf(mac, sizeof(mac), "%02x:%02x:%02x:%02x:%02x:%02x",
-                 m[0], m[1], m[2], m[3], m[4], m[5]);
-        if (local_count < MAX_LOCAL)
-            strncpy(local_addr[local_count].mac, mac, sizeof(local_addr[local_count].mac) - 1);
-        /* MAC is checked separately below; keep it in a simple static slot. */
-    }
-    close(fd);
-}
-
-static char local_macs[MAX_LOCAL][32];
-static int local_mac_count;
-
-static void collect_if(const char *ifname)
-{
-    int fd;
-    struct ifreq ifr;
-    char ip[INET_ADDRSTRLEN];
-    unsigned char *m;
-    int i;
-
-    if (!ifname || !*ifname) return;
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (fd < 0) return;
     memset(&ifr, 0, sizeof(ifr));
     strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
     if (ioctl(fd, SIOCGIFADDR, &ifr) == 0) {
         struct sockaddr_in *sin = (struct sockaddr_in *)&ifr.ifr_addr;
         if (inet_ntop(AF_INET, &sin->sin_addr, ip, sizeof(ip))) add_local_ip(ip);
     }
+
     memset(&ifr, 0, sizeof(ifr));
     strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
     if (ioctl(fd, SIOCGIFHWADDR, &ifr) == 0) {
         m = (unsigned char *)ifr.ifr_hwaddr.sa_data;
         if (m[0] || m[1] || m[2] || m[3] || m[4] || m[5]) {
-            snprintf(local_macs[local_mac_count], sizeof(local_macs[0]),
-                     "%02x:%02x:%02x:%02x:%02x:%02x",
+            snprintf(mac, sizeof(mac), "%02x:%02x:%02x:%02x:%02x:%02x",
                      m[0], m[1], m[2], m[3], m[4], m[5]);
             for (i = 0; i < local_mac_count; i++)
-                if (!strcmp(local_macs[i], local_macs[local_mac_count])) { close(fd); return; }
-            if (local_mac_count < MAX_LOCAL - 1) local_mac_count++;
+                if (!strcasecmp(mac, local_macs[i])) { close(fd); return; }
+            if (local_mac_count < MAX_LOCAL) {
+                strncpy(local_macs[local_mac_count], mac, sizeof(local_macs[0]) - 1);
+                local_macs[local_mac_count][sizeof(local_macs[0]) - 1] = 0;
+                local_mac_count++;
+            }
         }
     }
     close(fd);
@@ -114,8 +81,8 @@ static int is_local_line(const char *line)
         if (!q) q = p + strlen(p);
         if ((size_t)(q - p) < sizeof(ip)) {
             memcpy(ip, p, (size_t)(q - p)); ip[q - p] = 0;
-            for (i = 0; i < local_count; i++)
-                if (!strcmp(ip, local_addr[i].ip)) return 1;
+            for (i = 0; i < local_ip_count; i++)
+                if (!strcmp(ip, local_ips[i])) return 1;
         }
     }
     p = strstr(line, " MAC=");
@@ -150,7 +117,7 @@ int main(int argc, char **argv)
 
     if (pipe(pipefd) < 0) return 1;
     pid = fork();
-    if (pid < 0) return 1;
+    if (pid < 0) { close(pipefd[0]); close(pipefd[1]); return 1; }
     if (pid == 0) {
         close(pipefd[0]);
         dup2(pipefd[1], STDOUT_FILENO);
