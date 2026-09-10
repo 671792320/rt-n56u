b0289d8d912f75fc17120c8913967cef4db4a851
671792320 <671792320@qq.com>
2026-09-11 07:52:42 +0800

    修改Q7默认WiFi SSID为IPCTEST-UTP-T2格式

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -16,7 +16,7 @@
  */
 
 #ifndef _defaults_h_
-#define _defaults_h_
+#define _defaults_h
 
 #include <ralink_board.h>
 
@@ -39,8 +39,8 @@
 #define DEF_WLAN_5G_CC		"US"
 
 #if defined(BOARD_ZTE_Q7)
-/* Q7使用设备LAN MAC最后四位生成默认无线名称。 */
-#define DEF_WLAN_2G_SSID	"@Seetong-IPCtest-utp2_%s"
+/* Q7默认无线名称：前缀 + WiFi MAC最后三位，避免SSID过长。 */
+#define DEF_WLAN_2G_SSID	"@Seetong-IPCTEST-UTP-T2_%s"
 #define DEF_WLAN_2G_PSK		"88888888"
 #else
 #define DEF_WLAN_2G_SSID	BOARD_PID "_%s"
